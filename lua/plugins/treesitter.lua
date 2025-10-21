7519b2e3d5164f911805ccc260f5bfa13f15496b
Jack Coutts <jackwgcoutts@proton.me>
2025-10-21 13:54:54 +0100

    updates

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "cpp",
+      "javascript",
+      "json",
+      "lua",
+      "python",
+      "typescript",
+      "tsx",
+      "yaml",
+      "markdown",
+      "markdown_inline",
+    },
+  },
+}
